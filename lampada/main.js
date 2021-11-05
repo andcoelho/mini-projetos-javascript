03e1ed9aa244f89ff8260e0c15d6682d9094e4fd
Anderson <andcoelho97@gmail.com>
2021-11-05 13:34:01 -0300

    lampada

---
+++ lampada/main.js
@@ -0,0 +1,9 @@
+const lamp = window.document.getElementById("lampada");
+
+function ligar() {
+    lampada.src = './img/lamp-ligada.jpg';
+}
+
+function desligar() {
+    lampada.src = './img/lamp-desligada.jpg';
+}
